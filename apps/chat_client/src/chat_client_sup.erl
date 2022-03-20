fe523649ebfe00312164f93d814310293f68d56b
rs <r.salerno@reply.it>
2022-03-20 20:13:03 +0100

    update sup

--- apps/chat_client/src/chat_client_sup.erl
+++ apps/chat_client/src/chat_client_sup.erl
@@ -16,20 +16,12 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
 init([]) ->
     SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
-    ChildSpecs = [],
+                 intensity => 1,
+                 period => 5},
+    ChildSpecs = [
+      #{id => chat_client_tcp, 
+        start => {chat_client_tcp, start, []}}
+      ],
     {ok, {SupFlags, ChildSpecs}}.
-
-%% internal functions
